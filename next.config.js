a7d891d6826c437cb61349eb04a1a02e654c22cb
AgistStudio <junior.ho@gmail.com>
2024-07-11 15:43:25 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,23 @@
-const path = require('path')
+const withPWA = require("next-pwa");
 
-module.exports = {
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
+module.exports = withPWA({
+  // module.exports = {
+  i18n: {
+    locales: ["en", "my"],
+    defaultLocale: "en",
   },
-}
+  reactStrictMode: true,
+  // swcMinify: true,
+  compiler: {
+    removeConsole: true,
+  },
+  images: {
+    domains: ["robohash.org", "res.cloudinary.com"],
+  },
+  pwa: {
+    dest: "public",
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+  },
+  // };
+});
